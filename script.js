dc40e439c4e54da28c6e286523b0552fc0f061bb
masa <tosa9629@gmail.com>
2021-11-29 02:25:53 -0700

    contact section done, created _keyframes.scss, icons in home section was changed, style for skills section was changed

--- script.js
+++ script.js
@@ -17,24 +17,20 @@ for (var i = 0; i < tabs.length; i++)  {
   })
 };
 
-var classImgs = document.querySelectorAll('.works-img')
-var madeOf = document.querySelectorAll('.made-of')
-console.log(classImgs, madeOf)
-for (let i = 0; i < classImgs.length; i++)  {
-  classImgs[i].addEventListener('toggle', function(){
-    for (var i = 0; i < madeOf.length; i++)  {
-      eachYearClass[i].className = 'year'
-    }
-    document.getElementById(this.dataset.id).className = 'year active';
-    for (var i = 0; i < tabs.length; i++)  {
-      tabs[i].className = 'tab-wrapper'
-    }
-    this.className = 'tab-wrapper active';
-  })
-};
+var emailAddress = document.getElementById('emailAddress')
+var emailIcons = document.getElementsByClassName('fa-paper-plane')
+var calendly = document.getElementById('calendly')
+var calendlyIcons = document.getElementsByClassName('fa-calendar-alt')
+console.log({emailAddress, emailIcons, calendly, calendlyIcons})
 
-
-
-
+// emailAddress.addEventListener('mouseover', function(){
+//   for (let i = 0; i < emailIcons.length; i++)  {
+//     if (emailIcons[i].classList.contains('active')){
+//       emailIcons[i].className = 'fa-paper-plane'
+//       emailIcons[i+1].className = 'fa-paper-plane active'
+//       console.log(emailIcons)
+//     }
+//   }
+// })
 
 
